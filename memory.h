c56da7c1873807097aac4dfaac5debd08682acbd
Robert Peters <robert-peters@gmx.net>
2012-10-10 22:02:49 +0200

    memory: added handler for mem reads and writes
    Needed for controller handling. Don't know why
    they did this that way. Ok, memory was very
    expensive...

--- memory.h
+++ memory.h
@@ -2,10 +2,17 @@
 
 #include <stdint.h>
 #include <vector>
+#include <map>
 
 class Memory
 {
 public:
+	class Handler
+	{
+		virtual void on_mem_read(uint16_t addr, uint16_t value) = 0;
+		virtual void on_mem_write(uint16_t addr, uint16_t value) = 0;
+	};
+
 	typedef uint8_t mem_t
 
 	Memory(uint16_t size)
@@ -13,11 +20,29 @@ public:
 	{}
 
 	mem_t read(uint16_t addr) const
-	{ return _memory[addr]; }
+	{
+		const mem_t mem = _memory[addr];
+
+		if(_handlers.find(addr) != std::map<uint16_t, Handler*>::end())
+			_handlers[addr].on_mem_read(addr, mem);
+
+		return mem;
+	}
 
 	void write(uint16_t addr, mem_t value)
-	{ _memory[addr] = value; }
+	{
+		_memory[addr] = value;
+
+		if(_handlers.find(addr) != std::map<uint16_t, Handler*>::end())
+			_handlers[addr].on_mem_write(addr, value);
+	}
+
+	void reg_mem_read_handler(uint16_t addr, Handler *hdl)
+	{
+		_handlers[addr] = hdl;
+	}
 
 private:
 	std::vector<mem_t> _memory;
+	std::map<uint16_t, Handler*> _handlers;
 };
